dd49e3202f768bc3d1f07f8d2c31c9dcb6b1372b
sodahub-64 <sotarockyz@icloud.com>
2026-01-01 01:00:03 +0900

    chore: scaffold astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://sodahub99k.github.io',
+  base: process.env.GITHUB_PAGES ? '/yearshift' : '/',});
